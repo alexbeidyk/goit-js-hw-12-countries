fbd7048a579f8b2129c4bd0e615890ee29e4d232
Alexandr Beidyk <65552941+alexbeidyk@users.noreply.github.com>
2020-12-29 21:46:12 +0200

    Сompiled using webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import './js/refs.js';
+import './js/fetchCountries.js';
+import './js/markup.js';
+import './js/pnotify';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/core/dist/BrightTheme.css';
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+  formCountryName: document.querySelector('#formCountryName'),
+  formCountryNameInput: document.querySelector('#formCountryNameInput'),
+  searchResult: document.querySelector('#searchResult'),
+};
+
+export default refs;
